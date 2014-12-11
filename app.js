7c85592e46ccc972c83aee79c735052e1f3a0b9f
AlmogBaku <almog.baku@gmail.com>
2014-12-11 23:23:59 +0200

    fixes "package not found error" add repo to the package

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-'use strict';
-
-/*
- * Defining the Package
- */
-var Module = require('meanio').Module;
-
-var meanCompass = new Module('meanCompass');
-
-/*
- * All MEAN packages require registration
- * Dependency injection is used to define required modules
- */
-meanCompass.register(function(app, auth, database) {
-  return meanCompass;
-});
+server
